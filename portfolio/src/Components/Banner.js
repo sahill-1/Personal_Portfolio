c16a8883c263777faab583ce60cb850f2be14012
sahill-1 <sv260397@gmail.com>
2023-05-19 15:26:57 +0530

    main banner three six

--- portfolio/src/Components/Banner.js
+++ portfolio/src/Components/Banner.js
@@ -1,6 +1,37 @@
-import { Container, Row, Col } from "react-bootstrap"
-import {ArrowRightCIrcle} from "react-bootstrap-icons"
+import {useState, useEffect} from "react"
+import { Container, Row, Col } from "react-bootstrap";
+import {ArrowRightCircle} from "react-bootstrap-icons";
+import headerImg from "../assets/img/header-img.svg";
+
 export const  Banner = () => {
+
+    const [loopNum, setLoopNum] = useState(0);
+    const [isDeleting, isSetDeleting] = useState(false);
+    const toRotate = ["Frontend Developer", "Web Designer"];
+    const [text, setText] = useState(" ");
+    const [delta, setDelta] = useState(300 - Math.random() * 100);
+    const period = 2000;
+
+    useEffect(() => {
+        let ticker = setInterval(() => {
+            tick()
+        }, delta)
+
+        return () => {clearInterval(ticker)};
+    }, [text])
+
+    const tick = () => {
+        let i = loopNum % toRotate.length;
+        let fullText = toRotate(i);
+        let updatedText = isDeleting ? fullText.substring(0, text.length-1) : fullText.substring(0, text.length+1);
+        
+        setText(updatedText);
+
+        if (isDeleting){
+            
+        }
+    }
+
     return (
         <section className="banner" id="home">
             <Container>
@@ -9,10 +40,10 @@ export const  Banner = () => {
                         <span className="tagline">Welcome to my Portfolio!</span>
                         <h1>{`Hi, I'm Sahil.`}<span className="wrap">Frontend Developer</span></h1>
                         <p>Hi, mt name is  Sahil Vishwakarma and I am currentlly working as a Frontend Developer at Business World Trade.</p>
-                        <button onClick={() => console.log("Connect")}>Let's Connect <ArrowRightCIrcle size={25}/></button>
+                        <button onClick={() => console.log("Connect")}>Let's Connect <ArrowRightCircle size={25}/></button>
                     </Col>
                     <Col xs={12} md={6} xl={5}>
-                        <img src="" alt="Header-img" />
+                        <img src={headerImg} alt="Header-img" />
                     </Col>
                 </Row>
             </Container>
