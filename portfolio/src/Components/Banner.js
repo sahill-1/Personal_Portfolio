377a49758a05afd1683fd784408dfd825738b9a0
sahill-1 <sv260397@gmail.com>
2023-05-23 11:57:44 +0530

    banner styling

--- portfolio/src/Components/Banner.js
+++ portfolio/src/Components/Banner.js
@@ -6,7 +6,7 @@ import headerImg from "../assets/img/header-img.svg";
 export const  Banner = () => {
 
     const [loopNum, setLoopNum] = useState(0);
-    const [isDeleting, isSetDeleting] = useState(false);
+    const [isDeleting, setIsDeleting] = useState(false);
     const toRotate = ["Frontend Developer", "Web Designer"];
     const [text, setText] = useState(" ");
     const [delta, setDelta] = useState(300 - Math.random() * 100);
@@ -22,13 +22,22 @@ export const  Banner = () => {
 
     const tick = () => {
         let i = loopNum % toRotate.length;
-        let fullText = toRotate(i);
+        let fullText = toRotate [i];
         let updatedText = isDeleting ? fullText.substring(0, text.length-1) : fullText.substring(0, text.length+1);
         
         setText(updatedText);
 
         if (isDeleting){
-            
+            setDelta(prevDelta => prevDelta/2);
+        }
+        if(!isDeleting && updatedText === fullText){
+            setIsDeleting(true);
+            setDelta(period)
+        }
+        else if(!isDeleting && updatedText === " "){
+            setIsDeleting(false);
+            setLoopNum(loopNum+1);
+            setDelta(500)
         }
     }
 
@@ -38,7 +47,7 @@ export const  Banner = () => {
                 <Row className="align-items-center">
                     <Col xs={12} md={6} xl={7}>
                         <span className="tagline">Welcome to my Portfolio!</span>
-                        <h1>{`Hi, I'm Sahil.`}<span className="wrap">Frontend Developer</span></h1>
+                        <h1>{`Hi, I'm Sahil. `}<span className="wrap">{text}</span></h1>
                         <p>Hi, mt name is  Sahil Vishwakarma and I am currentlly working as a Frontend Developer at Business World Trade.</p>
                         <button onClick={() => console.log("Connect")}>Let's Connect <ArrowRightCircle size={25}/></button>
                     </Col>
